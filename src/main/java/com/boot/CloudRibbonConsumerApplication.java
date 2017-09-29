f3185fd579de7de56d40e30ec80e4c32a0993b88
admin <admin@DESKTOP-VVLNJUU>
2017-09-29 15:02:46 +0800

    22222

--- src/main/java/com/boot/CloudRibbonConsumerApplication.java
+++ src/main/java/com/boot/CloudRibbonConsumerApplication.java
@@ -2,11 +2,14 @@ package com.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+//断路器配置
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class CloudRibbonConsumerApplication {
--- src/main/java/com/boot/controller/ConsumerController.java
+++ src/main/java/com/boot/controller/ConsumerController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.boot.service.HelloService;
+
 /**
  * 实现 /ribbon-consumer接口
  * 在这里 将使用 RestTemplate 来实现对Hello-server服务提供者提供的/hello
@@ -13,13 +15,35 @@ import org.springframework.web.client.RestTemplate;
  *
  */
 @RestController
+
 public class ConsumerController {
 
-	@Autowired
+	//第一版， 直接测试调用
+	/*@Autowired
 	RestTemplate restTemplate;
 	
 	@GetMapping("/ribbon-consumer")
 	public String helloConsumer(){
 		return restTemplate.getForEntity("http://hello-service/hello", String.class).getBody();
+	}*/
+	
+	
+	//调用helloservice
+	@Autowired
+	HelloService helloService;
+	
+	
+	@GetMapping("/ribbon-consumer")
+	public String helloConsumer() {
+		return helloService.helloService();
 	}
+	
 }
+
+
+
+
+
+
+
+
